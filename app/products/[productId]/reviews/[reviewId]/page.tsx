af10dc2e9e93c201b97646d294bb76eb70b94015
Vipul Tyagi <vipultyagiabes@gmail.com>
2025-02-16 13:39:43 +0530

    NOT FOUND in navigation:: add a default not found page

--- app/products/[productId]/reviews/[reviewId]/page.tsx
+++ app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { notFound } from 'next/navigation';
 
 async function Review({params} : {params : Promise<{productId: string; reviewId : string;}>}) {
 
     const pId = (await params).productId;
     const rId = (await params).reviewId;
+    if (parseInt(rId) > 100) {
+        notFound(); // Redirects to the 404 page
+    }
     
   return (
     <div>Product {pId} with  reviewId {rId}</div>
