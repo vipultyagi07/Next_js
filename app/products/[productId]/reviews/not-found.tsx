d67aae814315ff874e088a1e59952b71a91bd3d1
Vipul Tyagi <vipultyagiabes@gmail.com>
2025-02-16 14:08:48 +0530

    CUSTOM NOT FOUND in navigation:: add a default not found page

---
+++ app/products/[productId]/reviews/not-found.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function notFound() {
+  return (
+    <div>
+        <h1>Please Enter correct review id <br></br> it should be under 100</h1>
+
+    </div>
+  )
+}
+
+export default notFound
