68344358054651f82828ebc0d728ac1f1aa50df6
Vipul Tyagi <vipultyagiabes@gmail.com>
2025-02-16 13:04:26 +0530

    NOT FOUND:: add a default not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function notFound() {
+  return (
+    <div>
+        <h1>The url is wrong please <br></br> Please put correct url</h1>
+
+    </div>
+  )
+}
+
+export default notFound
