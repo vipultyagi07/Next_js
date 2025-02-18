6ec2846860e21d4da35ad40d9cc5623c66f0d950
Vipul Tyagi <vipultyagiabes@gmail.com>
2025-02-18 10:32:00 +0530

    Dynamic Link Component :: BAD Approach

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import Link from 'next/link'
 
 function ProductPage() {
   return (
     <div>
     <h1>Product List</h1>
-    <h1>Product  1</h1>
-    <h1>Product  2</h1>
-    <h1>Product  3</h1>
+    <h1> <Link href={"/products/1"}>Product  1</Link></h1>
+    <h1> <Link href={"/products/2"}>Product  2</Link></h1>
+    <h1> <Link href={"/products/3"}>Product  3</Link></h1>
     </div>
 
   )
