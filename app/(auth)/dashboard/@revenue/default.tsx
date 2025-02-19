322c29e40fbcafba750a415458b2494ffa3bfec7
Vipul Tyagi <vipultyagiabes@gmail.com>
2025-02-19 21:02:01 +0530

    SUB NAVIGATION :: good

---
+++ app/(auth)/dashboard/@revenue/default.tsx
@@ -0,0 +1,10 @@
+import { Card } from '@/app/component/card'
+import React from 'react'
+
+function revenue() {
+  return (
+    <Card>Default revenue</Card>
+  )
+}
+
+export default revenue
---
+++ app/(auth)/dashboard/default.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function DashboardFun() {
+  return (
+    <div className="p-4">
+      <h2 className="text-2xl font-bold text-gray-800 dark:text-white">
+        Welcome to the Default Dashboard
+      </h2>
+      <p className="text-gray-600 dark:text-gray-300">
+        Here you can view your default stats, notifications, and more.
+      </p>
+    </div>
+  );
+}
+
+export default DashboardFun;
